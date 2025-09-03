a033aee191a0911b9fb1faed3103ff3d1ab13658
Nikilehsa <nikilesha.nivas@gmail.com>
2025-09-04 01:20:22 +0530

    created the navbar plus colours

---
+++ components/Navbar.jsx
@@ -0,0 +1,18 @@
+import logo from '../src/assets/logo.png';
+
+const navbar = () => {
+  return (
+    <>
+    <div className='h-16 w-full bg-[var(--color-navbar)] flex items-center px-6'>
+        <img src={logo} alt="Website Logo" className='h-full w-auto ml-5' />
+      </div>
+    <div className='min-h-screen w-full bg-[var(--color-background)]'>
+
+    
+      
+    </div>
+    </>
+  )
+}
+
+export default navbar
